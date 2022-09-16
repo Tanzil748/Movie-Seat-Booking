27fdece671689cc784239e8df3a3012ff5a9484b
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-09-16 17:41:52 -0400

    Added clear data functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+const clearSeats = document.querySelector(".btn");
 
 populateUI();
 
@@ -47,6 +48,22 @@ function populateUI() {
   }
 }
 
+// Function resets local storage
+function resetSeats() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  localStorage.removeItem("selectedSeats");
+  location.reload();
+}
+
 // Movie Select Event Listener
 movieSelect.addEventListener("change", function (e) {
   ticketPrice = +e.target.value;
@@ -64,6 +81,11 @@ container.addEventListener("click", function (e) {
 
     updateSelectedCount();
   }
+});
+
+// Clear seats event listener
+clearSeats.addEventListener("click", function (e) {
+  resetSeats();
 });
 
 // Initial count and total set
